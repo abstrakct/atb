00f69ac66e65771d4331ca4d289ea78cb63d97f7
Rolf K <abstractstar@gmail.com>
2017-06-12 20:06:59 +0200

    updated gitignore

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define VERSION_MAJOR cde40ff-dirty
+#define VERSION_MAJOR 177e2a9-dirty
 #define VERSION_MINOR 
 #define VERSION_REVISION 
 #define VERSION_HASH ""
-#define VERSION_STRING "cde40ff-dirty..."
+#define VERSION_STRING "177e2a9-dirty..."
 #endif
